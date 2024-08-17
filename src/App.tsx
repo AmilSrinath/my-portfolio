e8888e62c32ac91cb45d1cfc8869eaf3a7f65c06
AmilSrinath <amilsrinath5@gmail.com>
2024-08-17 19:59:32 +0530

    create home

--- src/App.tsx
+++ src/App.tsx
@@ -1,36 +1,46 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
+import { BrowserRouter } from "react-router-dom";
+import {
+    About,
+    Contact,
+    Experience,
+    Feedbacks,
+    Hero,
+    Navbar,
+    Tech,
+    Works,
+    StarsCanvas,
+} from "./components";
+import Banner from "./components/banner";
+import Footer from "./components/footer";
 
-function App() {
-  const [count, setCount] = useState(0)
+// App
+const App = () => {
+    const [hide, setHide] = useState(true);
 
-  return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-        <h1>Amil</h1>
-    </>
-  )
-}
+    return (
+        <BrowserRouter>
+            <Banner hide={hide} setHide={setHide} />
+            <div className="relative z-0 bg-primary">
+                <div className="bg-hero-pattern bg-cover bg-no-repeat bg-center">
+                    <Navbar hide={hide} />
+                    <Hero />
+                </div>
+                <About />
+                <Experience />
+                <Tech />
+                <Works />
+                <Feedbacks />
 
-export default App
+                {/* Contact */}
+                <div className="relative z-0">
+                    <Contact />
+                    <StarsCanvas />
+                </div>
+                <Footer />
+            </div>
+        </BrowserRouter>
+    );
+};
+
+export default App;
--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,23 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
+import * as React from "react";
+import ReactDOM from "react-dom/client";
+import { Toaster } from "sonner";
 
-createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+import App from "./App.tsx";
+
+import "./index.css";
+
+const rootEl = document.getElementById("root");
+
+ReactDOM.createRoot(rootEl!).render(
+    <React.StrictMode>
+        <Toaster
+            theme="dark"
+            richColors
+            closeButton
+            toastOptions={{
+                style: { background: "#050816", opacity: 0.95 },
+            }}
+        />
+        <App />
+    </React.StrictMode>,
+);
